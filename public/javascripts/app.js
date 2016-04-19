2fe7020756dd176a6946a152def867182062dcb6
unknown <vindexus@gmail.com>
2016-04-19 02:11:59 -0700

    Starting putting in CRUD UI for follows. Weird mix of express and angular app.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,17 +3,30 @@ angular.module('app', ['ngResource', 'ngAnimate'])
       return {
         get: function (params) {
           return $http({method: 'GET', url: '/followers', params: params});
+        },
+        delete: function(id) {
+          return $http({method: 'DELETE', url: '/followers/' + id});
         }
       }
     }])
 .controller('FollowsController', ['$scope', 'Followers', '$timeout', function ($scope, Followers, $timeout) {
+  $scope.removeFollow = function(id) {
+    if(!confirm("Remove follow?")) {
+      return;
+    }
+    Followers.delete(id);
+  };
+
+  console.log("FollowsController");
+}])
+.controller('FollowsDisplayController', ['$scope', 'Followers', '$timeout', function ($scope, Followers, $timeout) {
   $scope.followers = [];
 
   function fetchNext() {
     console.log("most recent _id for " + $scope.followers[0].follower, $scope.followers[0]._id);
     console.log("one after that for " + $scope.followers[1].follower, $scope.followers[1]._id);
     console.log($scope.followers[0]._id + " > " + $scope.followers[1]._id  +" is ", $scope.followers[0]._id > $scope.followers[1]._id)
-    Followers.get({limit: 1, after: $scope.followers[0]._id}).success(function (data) {
+    Followers.get({limit: 1, after: $scope.followers[0]._id, orderBy: '_id', desc: 0}).success(function (data) {
       if(data.length > 0) {
         $scope.followers.unshift(data[0]);
       }
@@ -28,4 +41,7 @@ angular.module('app', ['ngResource', 'ngAnimate'])
       $timeout(fetchNext, $scope.followsDisplayTime);
     });
   });
+}])
+.controller('EditFollowController', ['$scope', 'Followers', function($scope, Followers) {
+  $scope.follow = Followers.get($scope.followId)
 }]);
--- routes/followers.js
+++ routes/followers.js
@@ -3,12 +3,23 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 var Follower = require('../models/Follower.js');
+var config = require('../config/config.js');
 
 router.get('/', function(req, res, next) {
-  var limit = req.query.hasOwnProperty("limit") ? req.query.limit : 5;
+  var limit = req.query.hasOwnProperty("limit") ? req.query.limit : 20;
   var skip = req.query.hasOwnProperty("skip") ? req.query.skip : 0;
-  var sort = req.query.hasOwnProperty("sort") ? req.sort : {_id: -1};
-  var where = {}; 
+  var sort = {_id: -1};
+  var where = {};
+  var page = req.query.hasOwnProperty("page") ? req.query.page : 1;
+
+  if(req.query.hasOwnProperty("orderBy")) {
+    sort[req.query.orderBy] = req.query.hasOwnProperty("desc") ? (req.query.desc == true ? -1 : 1) : 1;
+  }
+
+  if(req.query.hasOwnProperty("page")) {
+    skip = (page - 1) * limit;
+  }
+  //console.log("req", req);
 
   if(req.query.hasOwnProperty("type")) {
     where.type = req.query.type;
@@ -20,25 +31,46 @@ router.get('/', function(req, res, next) {
 
   Follower.find(where, function (err, followers) {
     if (err) return next(err);
-    res.json(followers);
+    res.format( {
+      json: function () {
+        res.json(followers); 
+      },
+      html: function () {
+        res.render('followers/index', {title: "Followers Page " + page, config: config, followers: followers});
+      }
+    });
   }).limit(limit).sort(sort).skip(skip);
 });
 
 /* GET /todos listing. */
 router.get('/:id', function(req, res, next) {
-  Follower.findById(req.params.id, function (err, follower) {
+  Follower.findById(req.params.id, function (err, follow) {
     if (err) return next(err);
-    res.json(follower);
+    res.format( {
+      json: function () {
+        res.json(follow);
+      },
+      html: function () {
+        res.render('followers/edit', {follow: follow, title: 'Edit Follow'});
+      }
+    });
   });
 });
 
-router.put('/:id', function(req, res, next) {
+router.post('/:id', function(req, res, next) {
   Follower.findByIdAndUpdate(req.params.id, req.body, function(err,post) {
     if(err) {
       return next(err);
     }
 
-    res.json(post)
+    res.format( {
+      json: function () {
+        res.json(post);
+      },
+      html: function () {
+        res.redirect('/followers/' + req.params.id);
+      }
+    });
   });
 });
 
